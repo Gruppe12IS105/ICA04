d75a98c6e3761c3d33c60f3c0d3cf7ccd7169310
nicolz13 <poofnut@hotmail.com>
2017-03-20 17:26:18 +0100

    Oppg2 update

--- Oppg 2/fileinfo.go
+++ Oppg 2/fileinfo.go
@@ -17,7 +17,9 @@ func main() {
 	kib := b / 1024
 	mib := kib / 1024
 	gib := mib / 1024
-	fmt.Println("Navn:", filnavn)
+
+	fmt.Println("------------------------------------")
+	fmt.Println("Information about a file:", filnavn)
 	fmt.Println("Size:", b, "bytes,", kib, "kibibytes,", mib, "mibibytes,", gib, "gibibytes")
 
 	if file.Mode().IsDir() == true {
@@ -31,24 +33,40 @@ func main() {
 		fmt.Println("Is not a regular file")
 	}
 	fmt.Println("Has Unix permissions bits: ", file.Mode().Perm())
-
-	//
-	// 	switch mode := file.Mode(); {
-	// 	case mode.IsDir():
-	// 			fmt.Println("directory", file.Mode().IsDir())
-	// 	case mode.IsRegular():
-	// 		fmt.Println("regular file", file.Mode().IsRegular())
-	// 	case mode&os.ModePerm != 0:
-	// 		fmt.Println("Has Unix permissions bits: ", file.Mode().Perm())
-	// 	case mode&os.ModeAppend != 0:
-	// 		fmt.Println("append only")
-	// 	case mode&os.ModeDevice != 0:
-	// 		fmt.Println("device file")
-	// 	case mode&os.ModeCharDevice != 0:
-	// 		fmt.Println("Unix character device")
-	// //	case ----
-	// //		fmt.Println("Unix block device")
-	// 	case mode&os.ModeSymlink != 0:
-	// 		fmt.Println("symbolic link")
-	// 	}
+	if file.Mode()&os.ModeAppend == os.ModeAppend {
+		fmt.Println("Is append only")
+	} else {
+		fmt.Println("Is not append only")
+	}
+	if file.Mode()&os.ModeDevice == os.ModeDevice {
+		fmt.Println("Is a device file")
+	} else {
+		fmt.Println("Is not a device file")
+	}
+	if file.Mode()&os.ModeCharDevice == os.ModeCharDevice {
+		fmt.Println("Is a Unix character device")
+	} else {
+		fmt.Println("Is not a Unix character device")
+	}
+//	if file.Mode()
+//		fmt.Println("Unix block device")
+	if file.Mode()&os.ModeSymlink != 0 {
+		fmt.Println("Is a symbolic link")
+	} else {
+		fmt.Println("Is not a symbolic link")
+	}
+	fmt.Println("------------------------------------")
 }
+
+// 	switch mode := file.Mode(); {
+// 	case mode&os.ModeAppend != 0:
+// 		fmt.Println("append only")
+// 	case mode&os.ModeDevice != 0:
+// 		fmt.Println("device file")
+// 	case mode&os.ModeCharDevice != 0:
+// 		fmt.Println("Unix character device")
+// //	case ----
+// //		fmt.Println("Unix block device")
+// 	case mode&os.ModeSymlink != 0:
+// 		fmt.Println("symbolic link")
+// 	}
