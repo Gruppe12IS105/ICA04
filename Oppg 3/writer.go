02337c793f46bbea609a6e11bc22ff71cd349740
VasuraInDrag <indrag16@student.uia.no>
2017-03-21 23:43:01 +0100

    Write funksjoner

---
+++ Oppg 3/writer.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"bufio"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+//eksempler av flere typer write funksjoner.
+//Funksjonene skriver i write.txt filen, og ikke de i "files" mappen
+func main() {
+	write()
+	quickWrite()
+	bufferedWrite()
+}
+
+func write() {
+	//opens the write.txt file
+	file, err := os.OpenFile(
+		"write.txt",
+		os.O_WRONLY|os.O_TRUNC|os.O_CREATE,
+		0666,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	//Writes the bytes
+	byteSlice := []byte("Bytes!\n")
+	bytesWritten, err := file.Write(byteSlice)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Wrote %d bytes. \n", bytesWritten)
+}
+
+func quickWrite() {
+	err := ioutil.WriteFile("write.txt", []byte("This is quickwrite!\n"), 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func bufferedWrite() {
+	file, err := os.OpenFile("write.txt", os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	bufferedWriter := bufio.NewWriter(file)
+
+	bytesWritten, err := bufferedWriter.Write(
+		[]byte{65, 66, 67},
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Bytes written: %d\n", bytesWritten)
+
+	bytesWritten, err = bufferedWriter.WriteString(
+		"Buffered String\n",
+	)
+
+	unflushedBufferSize := bufferedWriter.Buffered()
+	log.Printf("Bytes buffered: %d\n", unflushedBufferSize)
+
+	bytesAvailable := bufferedWriter.Available()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Available buffer: %d\n", bytesAvailable)
+
+	bufferedWriter.Flush()
+
+	bufferedWriter.Reset(bufferedWriter)
+
+}
