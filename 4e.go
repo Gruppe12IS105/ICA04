03e49c95faffcd98c409d59fee46b4e4a58215d4
nicolz13 <poofnut@hotmail.com>
2017-03-21 18:22:29 +0100

    asdfg

---
+++ 4e.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+    "fmt"
+    "os"
+  )
+
+func main() {
+  fakultet := os.Args[1]
+  m := make(map[string]int)
+  m[""]
+  m[""]
+  m[""]
+  m[""]
+  m[""]
+  m[""]
+
+
+  // ENCODE
+  func encode(fakultet string) {
+
+    fmt.Println("")
+  }
+
+  // DECODE
+  func decode(fakultet int) {
+
+  }
+
+}
