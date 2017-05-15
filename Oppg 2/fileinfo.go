8d96de64d513f4f276871edf342abc9f8a2ef7d4
emilr15 <emilr15@uia.no>
2017-05-15 13:06:42 +0200

    Bla

--- Oppg 2/fileinfo.go
+++ Oppg 2/fileinfo.go
@@ -77,9 +77,9 @@ func main() {
 	}
 	fmt.Println("------------------------------------")
 
-	// TEST
+	///TEST///////////////////////////
 	fmt.Println("------Vil bli samme utskrift, men er litt alternativ koding----------")
-	fmt.Println("Information about a file:", filnavn)
+	fmt.Println("File name:", filnavn)
 	fmt.Println("Size:", b, "bytes,", kib, "kibibytes,", mib, "mibibytes,", gib, "gibibytes")
 	if file.Mode().IsDir() == true {
 		fmt.Println("Is a directory")
